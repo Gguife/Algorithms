2d43298e3a48a9c5aa5ea29ec2810eaf20bdf288
Gguife <gguife747@gmail.com>
2024-03-03 12:38:52 -0300

    solveMeFisrt resolved and making its algorithm complex

--- Exercise/1-solveMeFirst.js
+++ Exercise/1-solveMeFirst.js
@@ -20,13 +20,17 @@ function solveMeFisrtComplex(a, b) {
   //Calculate sum
   let sum = a + b;
 
-  //Rounding result
-  sum = parseFloat(sum.toFixed());
+  //Precision result
+  sum = parseFloat(sum.toFixed(2));
   
   return sum;
 }
-// Exemplo de uso da função
-const result1 = solveMeFisrtComplex(7, 3); // Sem precisão especificada
-console.log(result1); // Saída: 10
+// Example error
+//const resultError = solveMeFisrtComplex('maça', 'banana');
+//console.log(resultError); // error
+
+// Example function
+const result1 = solveMeFisrtComplex(7, 3); 
+console.log(result1); // 10
 const result2 = solveMeFisrtComplex(7.123, 3.456);
-console.log(result2); // Saída: 10.58
+console.log(result2); // 10.58
