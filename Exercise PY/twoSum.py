5d73a6d0e4f509c71e8e400aa26a81632c0e4770
Gguife <gguife747@gmail.com>
2024-03-11 18:59:10 -0300

    Resolved LeetCode TwoSum algorithm with Python

--- Exercise PY/twoSum.py
+++ Exercise PY/twoSum.py
@@ -1,6 +1,6 @@
 # LeetCode
 # Temos que encontrar dois numeros do nosso arr que sua soma seja igual o valor o valor pedido (target)
-# Objetivo é que o tempo de execuçãolinear O(n)
+# Objetivo é que o tempo de execução linear O(n)
 # Exemplo: nums = [2, 7, 11, 15] target = 9
 # output = [0,1] //2+7 = 9
 
