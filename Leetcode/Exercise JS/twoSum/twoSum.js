22a167252ebf7796a3d7da71b597b1817df2bb18
Gguife <gguife747@gmail.com>
2024-03-25 16:01:15 -0300

    TwoSum Leetcode exercise completed

--- Leetcode/Exercise JS/twoSum/twoSum.js
+++ Leetcode/Exercise JS/twoSum/twoSum.js
@@ -1,23 +1,23 @@
 /*
 LeetCode
-Neste exercicio temos que encontrar dois numeros do nosso arr que sua soma seja igual o valor o valor pedido (target)
+Neste exercicio temos que encontrar dois numeros do nosso arr que sua soma seja igual o valor pedido (target)
 Objetivo é que o tempo de execução seja linear O(n)
-Exemplo: nums = [2, 7, 11, 15] target = 9
+Exemplo: arr = [2, 7, 11, 15] target = 9
 output = [0,1] //2+7 = 9
 */
 
-const twoSum = (nums, target) =>{
+const twoSum = (arr, target) =>{
   let map = new Map();
-  for(let i = 0; i < nums.length; i++){
-    let complement = target - nums[i];
+  for(let i = 0; i < arr.length; i++){
+    let complement = target - arr[i];
     if(map.has(complement)){
       return [i, map.get(complement)]
     }
-    map.set(nums[i], i) //Setando o número e seu index quando o if nao da certo
+    map.set(arr[i], i)
   }
 };
 
-const nums = [2, 7, 11, 15];
+const arr = [7, 11, 15, 2];
 const target = 9;
-const result = twoSum(nums, target);
+const result = twoSum(arr, target);
 console.log(result)
