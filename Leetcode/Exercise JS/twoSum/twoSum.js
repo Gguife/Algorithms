0febb18fa07d76cc48cc4fa9cdd3134a91143693
Gguife <gguife747@gmail.com>
2024-03-27 11:11:34 -0300

    TwoSum algorithms leetcode

--- Leetcode/Exercise JS/twoSum/twoSum.js
+++ Leetcode/Exercise JS/twoSum/twoSum.js
@@ -21,3 +21,34 @@ const arr = [7, 11, 15, 2];
 const target = 9;
 const result = twoSum(arr, target);
 console.log(result)
+
+// OUTRAS FORMAS
+
+var twoSum2 = function(nums, target) {
+
+  const complimentToIndexMap = {}
+
+  for(i=0; i < nums.length; i++) {
+      const num = nums[i]
+      const compliment = target - num
+
+      if(complimentToIndexMap.hasOwnProperty(compliment)) {
+          return [complimentToIndexMap[compliment], i]
+      }
+
+      complimentToIndexMap[num] = i
+  }
+};
+
+
+var twoSum3 = function(nums, target) {
+  let res;
+  nums.forEach((num1, index1) => {
+      nums.forEach((num2, index2) => {
+          if(index1 != index2 && num1 + num2 == target) {
+              res = [index1, index2];
+          }
+      })
+  })
+  return res;
+};
