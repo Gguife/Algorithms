25592cf779950108cf0e06cd29902d538c2e3b62
Gguife <gguife747@gmail.com>
2024-05-16 18:58:46 -0300

    Hackerrank Breack Records exercise completed with JS

--- Hackerrank/Exercise JS/15-breakingRecords.js
+++ Hackerrank/Exercise JS/15-breakingRecords.js
@@ -1,14 +1,45 @@
 /*
 
+scores = [12, 24, 10, 24];
+As pontuaçoes estoa na mesma ordem dos jogos disputados. 
+
+A funcao deve retornar um array contendo o numero de vez que ela quebrou o recorde máximo e o numero  de vezes que ela quebour o record mínimo.
+
+Exemplo de entrada 0
+
+9 
+10 5 20 20 4 5 2 25 1
+
+Exemplo de saída 0
+2 4
 
 */
 
 
 function breakingRecords(scores){
+  let minGamesScore = scores[0];
+  let maxGamesScore = scores[0];
+  let resultMax = 0;
+  let resultMin = 0;
 
+  for(let i = 1; i < scores.length; i++){
+    if(scores[i] > maxGamesScore){
+      maxGamesScore = scores[i];
+      resultMax++;
+    }
+    else if(scores[i] < minGamesScore){
+      minGamesScore = scores[i];
+      resultMin++;
+    }
+
+  }
+
+  const result = [resultMax, resultMin];
+
+  return result; 
 }
 
-const scores = [12, 24, 10, 24];
+const scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
 breakingRecords(scores);
 
 
