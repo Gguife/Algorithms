13e48e85c3327b378321bb5fa81124669da6e334
Gguife <gguife747@gmail.com>
2024-08-23 16:05:43 -0300

    feat: Hackerrank exercise 16 solved

--- Hackerrank/Exercise JS/16-thebirthdayBar.js
+++ Hackerrank/Exercise JS/16-thebirthdayBar.js
@@ -1,12 +1,22 @@
 /* 
+Duas crianças querem dividir uma barra de chocolate.
 
+- A duração do segmento corresponde ao mês de nascimento de Ron
+- A soma dos números inteiros nos quadrados é igual ao dia do nascimento de lily.
 
+Exemplo:
+  s = [2, 2, 1, 3, 2];
+  d = 4;
+  m = 2;
+
+  supondo que o dia seja d = 4 e o mês m = 2, haveria duas possibilidades [2,2] e [1,3]
+  obs: A quantidade de seguimentos (no caso seria o mês "2") onde a soma dos números desse segmento tem que ser igual a o número do dia (dia = 4);
 */
 
 const birthday = (s, d, m) => {
   let result = 0;
 
-  for(let i = 0; i < s.length - m; i++){
+  for(let i = 0; i <= s.length - m; i++){
     let sum = 0;
 
     for(let j = 0; j < m; j++){
@@ -17,7 +27,7 @@ const birthday = (s, d, m) => {
       result++;
     }
   }
-
+  console.log(result);
   return result;
 }
 
