0d8b364a628c7bcf36309271bb2735576f32c715
Gguife <gguife747@gmail.com>
2025-01-30 18:21:41 -0300

    Count Valleys hackerrank exercise with JS

---
+++ Hackerrank/Exercise JS/23-countVallyes.js
@@ -0,0 +1,41 @@
+/*
+  Exemplo
+  steps = 8 = [DDUUUUDD];
+ 
+O caminhante entra primeiro em um valeunidades de profundidade. Então eles sobem e sobem em uma montanhaunidades de altura. Finalmente, o caminhante retorna ao nível do mar e termina a caminhada.
+
+Descrição da função
+
+countingValleys tem o(s) seguinte(s) parâmetro(s):
+
+  int steps : o número de passos na caminhada
+  string path : uma string que descreve o caminho
+  Devoluções
+
+  int: o número de vales percorridos
+  Formato de entrada
+
+  A primeira linha contém um inteiro, o número de passos na caminhada.
+  A segunda linha contém uma única string, decaracteres que descrevem o caminho.
+
+*/
+
+
+function countVallyes(steps, path) {
+  let altitude = 0;
+  let valleys = 0;
+
+  for(let i = 0; i < steps; i++) {
+    if(path[i] === 'U') {
+      altitude++;
+    }else if(path[i] === 'D') {
+      altitude--;
+    }
+
+    if(altitude === 0 && path[i] === 'U') {
+      valleys++;
+    }
+  }
+
+  return valleys;
+}
