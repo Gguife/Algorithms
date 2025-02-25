713196ea2d5df655dcbf0e13160a6efe31b80741
Gguife <gguife747@gmail.com>
2025-02-25 17:29:51 -0300

    Reverse Array exercise

---
+++ Hackerrank/Hackerrank-DS/reverseArray.js
@@ -0,0 +1,18 @@
+function reverseArray(arr) {
+  let reversedArray = [];
+  
+  for(let i = a.length - 1; i >= 0; i--) {
+      reversedArray.push(a[i]);
+  }
+    
+  return reversedArray;
+} 
+
+// Criar um variaval para armazenar o reverse array 
+// Criar um loop para reverse array
+  // i inicia com tamanho do array.length - 1 (inicia com o ultimo indice do array)
+  // i tem que ser maior ou igual 0
+  // i -- 
+  // realiza o push do ultimo indice no novo array ate o loop ser finalizado
+
+//Retorna o array reverse
